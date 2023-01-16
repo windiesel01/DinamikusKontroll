3d479550a94bc7ba6910b985838a62c7a5d5d5a0
cisco <cisco@B101-09>
2023-01-16 14:55:13 +0100

    Add project files.

---
+++ DinamikusKontroll/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DinamikusKontroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_tarolo = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel_tarolo
+            // 
+            this.panel_tarolo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel_tarolo.Location = new System.Drawing.Point(213, 0);
+            this.panel_tarolo.Name = "panel_tarolo";
+            this.panel_tarolo.Size = new System.Drawing.Size(846, 539);
+            this.panel_tarolo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 539);
+            this.Controls.Add(this.panel_tarolo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Alkalmazás ablak";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_tarolo;
+    }
+}
+
---
+++ DinamikusKontroll/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikusKontroll
+{
+    public partial class Form1 : Form
+    {
+        int sorokSzama = 3;
+        int oszlopokSzama = 6;
+        int pictureSize = 60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < oszlopokSzama; j++)
+                {
+                    Button button = new Button();
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.ImageLocation = "..//../images/muslim2.png";
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox.SetBounds(j * pictureSize, i * pictureSize, pictureSize, pictureSize);
+                    pictureBox.Text = (i*oszlopokSzama+j).ToString();
+                    pictureBox.Click += (o, ev) =>{
+
+                        PictureBox pictureBox1 = (PictureBox)o;
+                        pictureBox.ImageLocation = "..//../images/muslim.png";
+                    };
+                    panel_tarolo.Controls.Add(pictureBox);
+                }
+            }
+        }
+    }
+}
